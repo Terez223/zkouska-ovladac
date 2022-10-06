a3710e474bb69dd028fe3516c30a5cd9af218e52
Terez223 <114877792+Terez223@users.noreply.github.com>
2022-10-06 22:41:10 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(1)
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    radio.sendNumber(0)
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(4)
 })
 radio.setGroup(1)
 basic.showString("Hello!")
 joystickbit.initJoystickBit()
+basic.forever(function () {
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(0)
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendNumber(1)
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendNumber(2)
+    }
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        radio.sendNumber(3)
+    }
+})
